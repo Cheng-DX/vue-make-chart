1c47b07db7c7ccd6e910a3e678c48cba9a44c509
Cheng_DX <2337040896@qq.com>
2022-05-09 19:37:03 +0800

    feat: help icon

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
-import { NMessageProvider } from 'naive-ui'
+import { NDialogProvider, NMessageProvider } from 'naive-ui'
 import Main from '@/views/Main.vue'
 </script>
 
 <template>
   <n-message-provider placement="bottom-left">
-    <Main />
+    <n-dialog-provider>
+      <Main />
+    </n-dialog-provider>
   </n-message-provider>
 </template>
---
+++ src/components/helpContent/index.tsx
@@ -0,0 +1,18 @@
+import './index.css'
+import { NCarousel } from 'naive-ui'
+
+const helpContent = (
+  <div className="help-content-root">
+    <NCarousel showArrow draggable show-dots spaceBetween={20}>
+      <div>
+        <h3>Drag images into input(1/2)</h3>
+        <img src="src/assets/help1.jpg" className="helpImage" />
+      </div>
+      <div>
+        <h3>Type code is okay too(2/2)</h3>
+        <img src="src/assets/help2.jpg" className="helpImage" />
+      </div>
+    </NCarousel>
+  </div>
+)
+export default helpContent
