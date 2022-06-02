9afa9c56baf05dbe1466a46145c5bf466a6b5e94
duanxiaoxu03 <duanxiaoxu03@meituan.com>
2022-06-02 20:22:12 +0800

    feat: add icon

--- src/components/ConfigChart.vue
+++ src/components/ConfigChart.vue
@@ -17,23 +17,36 @@ const show = ref(Array(length).fill(false))
 function icon(show: boolean) {
   return show ? '' : 'icon-rotate'
 }
+
+function addProp() {
+  const key = prompt('key')
+  if (key) {
+    const value = prompt('value')
+    if (value)
+      props.root[key] = value
+  }
+}
 </script>
 
 <template>
   <div v-for="(key, index) in Object.keys(root)" :key="key" p-1rem>
-    <n-button
-      v-if="Object.is(Number(key), NaN)" secondary strong size="small" type="success"
-      @click="show[index] = !show[index]"
-    >
-      {{ key }}
-      <span :class="icon(show[index])" class="i-carbon-caret-down ml-0.3rem transition-transform duration-250ms" />
-    </n-button>
+    <div flex>
+      <n-button
+        v-if="Object.is(Number(key), NaN)" secondary strong size="small" type="success"
+        @click="show[index] = !show[index]"
+      >
+        {{ key }}
+        <span :class="icon(show[index])" class="i-carbon-caret-down ml-0.3rem transition-transform duration-250ms" />
+      </n-button>
+      <span i-carbon-add btn @click="addProp" />
+    </div>
+
     <div v-show="show[index]" mt-10px style="border-left: 1px dotted gray">
       <template v-if="Array.isArray(root[key])">
         <n-input-group v-if="!isObject(root[key][0])">
           <n-input v-for="(_, index) in root[key]" :key="`${key}-${index}`" v-model:value="root[key][index]" />
         </n-input-group>
-        <template v-for="(value, index) in root[key]" v-else :key="index">
+        <template v-for="(_, index) in root[key]" v-else :key="index">
           <n-divider dashed class="ml-0.5rem">
             {{ `${key}[${index}]` }}
           </n-divider>
@@ -53,7 +66,7 @@ function icon(show: boolean) {
         />
         <n-switch
           v-else-if="typeof root[key] === 'boolean'"
-          :value="root[key]"
+          v-model:value="root[key]"
         />
         <n-input
           v-else-if="typeof root[key] === 'string'"
